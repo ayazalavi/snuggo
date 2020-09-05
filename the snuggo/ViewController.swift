8ad779166dfcb753b44f3934dcf56a110a049a4b
Ayaz Alavi <ayaz.alavi@gmail.com>
2020-09-05 08:51:06 +0500

    Initial Commit

---
+++ the snuggo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  the snuggo
+//
+//  Created by Miranz  Technologies on 9/5/20.
+//  Copyright © 2020 Ayaz Alavi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
